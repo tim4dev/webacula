76b4cdba375deaad56802e9f08b48a4ad0219bc5
Yuri Timofeev <tim4dev@gmail.com>
2009-10-10 00:47:25 +0300

    improved design and small fix

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
  *
  */
 
-define('WEBACULA_VERSION', '3.3.0' . ', build 2009.10.09');
+define('WEBACULA_VERSION', '3.3.0' . ', build 2009.10.10');
 
 define('ROOT_DIR', dirname(dirname(__FILE__)) );
 error_reporting(E_ALL|E_STRICT);
