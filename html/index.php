eb0d55094a95530cc712219c449024526f106f14
Yuri Timofeev <tim4dev@gmail.com>
2009-10-12 09:29:21 +0300

    fix version build

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
  *
  */
 
-define('WEBACULA_VERSION', '3.3.0' . ', build 2009.10.10');
+define('WEBACULA_VERSION', '3.3.0' . ', build 2009.10.12');
 
 define('ROOT_DIR', dirname(dirname(__FILE__)) );
 error_reporting(E_ALL|E_STRICT);
