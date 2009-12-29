a5e4c5a19e9673d876aee8ff19b8384407f467ff
Yuri Timofeev <tim4dev@gmail.com>
2009-12-29 19:42:39 +0200

    regress test autochanger

--- tests/prepare_tests/prepare.sh
+++ tests/prepare_tests/prepare.sh
@@ -9,6 +9,7 @@ then
 	exit
 fi
 
+BCONSOLE="/sbin/bconsole"
 BACULADIR="etc/bacula"
 TMPDIR="/tmp/webacula"
 BASEDIR=`pwd`
@@ -47,6 +48,18 @@ my_check_log()
 }
 
 
+# check return code
+# exit if != 0
+my_check_rc() {
+    rc=$?
+    if test $rc -ne 0; then
+        echo "Return code = ${rc}"
+        exit
+    fi
+}
+
+
+
 
 #########################################################
 # Main program
@@ -68,7 +81,7 @@ if test $? -ne 0; then
    sleep 5
 fi
 
-chmod a+rx /sbin/bconsole
+chmod a+rx ${BCONSOLE}
 
 
 
@@ -82,6 +95,7 @@ mkdir "${TMPDIR}/tmp"
 mkdir "${TMPDIR}/log"
 mkdir "${TMPDIR}/restore"
 mkdir "${TMPDIR}/dev"
+mkdir "${TMPDIR}/dev/devchanger"
 mkdir "${TMPDIR}/test"
 mkdir "${TMPDIR}/test/1"
 mkdir "${TMPDIR}/test/2"
@@ -97,13 +111,14 @@ mv -f "/${BACULADIR}/bacula-sd.conf"  "/${BACULADIR}/bacula-sd.conf.original"
 mv -f "/${BACULADIR}/bacula-fd.conf"  "/${BACULADIR}/bacula-fd.conf.original"
 mv -f "/${BACULADIR}/bconsole.conf"   "/${BACULADIR}/bconsole.conf.original"
 
+# copy config files
 SRC="${BACULADIR}/*.conf"
 DST="/${BACULADIR}"
 echo "${SRC}"
 echo "${DST}"
-
 cp -f $SRC $DST
 
+# copy scripts
 SRC="${BACULADIR}/scripts/*.sh"
 DST="/${BACULADIR}/scripts/"
 mkdir  "${DST}"
@@ -126,15 +141,41 @@ sh ./webacula_postgresql_make_tables.sh
 sh ./webacula_sqlite_create_database.sh "/tmp/webacula/sqlite/webacula.db"
 chmod a+rwx /tmp/webacula/sqlite
 
-cd ${BASEDIR}
 
+my_log "Create virtual changer..."
+cd ${BASEDIR}
+my_check_rc
+cp -f dev/devchanger/*  "${TMPDIR}/dev/devchanger"
+my_check_rc
+cd ${TMPDIR}/dev/devchanger
+my_check_rc
+# create 75 volumes
+for i in `seq 1 75`; do
+    echo $i:vol$i >> barcodes
+    cp /dev/null slot$i
+done
+# make a cleaning tape
+echo 76:CLN01 >> barcodes
+cp /dev/null slot76
+# keep other empty
+for i in `seq 77 79`; do
+    echo $i:  >> barcodes
+done
+
+
+
+cd ${BASEDIR}
 
 my_log "Testing Configuration Files ..."
 
 /sbin/bacula-dir -t -c /${BACULADIR}/bacula-dir.conf
+my_check_rc
 /sbin/bacula-fd  -t -c /${BACULADIR}/bacula-fd.conf
+my_check_rc
 /sbin/bacula-sd  -t -c /${BACULADIR}/bacula-sd.conf
-/sbin/bconsole   -t -c /${BACULADIR}/bconsole.conf
+my_check_rc
+${BCONSOLE}   -t -c /${BACULADIR}/bconsole.conf
+my_check_rc
 
 rm -r -f ${TMPDIR}/tmp/*
 echo "Done."
@@ -145,21 +186,23 @@ my_log "Create dir tree ..."
 # Usage : <dir> <max files>
 cd ${BASEDIR}
 php ./create_dir_tree.php "${TMPDIR}/test/1" 3000
+my_check_rc
 dd if=/dev/zero of="${TMPDIR}/test/2/file21.dat"  bs=1024 count=1000
+my_check_rc
 dd if=/dev/zero of="${TMPDIR}/test/2/file22.dat"  bs=1024 count=500
+my_check_rc
 dd if=/dev/zero of="${TMPDIR}/test/3/file31.dat"  bs=1024 count=300
+my_check_rc
 dd if=/dev/zero of="${TMPDIR}/test/3/subdir/file_test41.dat" bs=1024 count=600
+my_check_rc
 dd if=/dev/zero of="${TMPDIR}/test/3/subdir/file_test42.dat" bs=1024 count=500
+my_check_rc
 
 
 my_log "Import data for Win32 backup ..."
 cp -f dev/pool.file.7d.0001 "${TMPDIR}/dev/"
 mysql -u root bacula < catalog/bacula.mysql.dump
-
-if test $? -ne 0; then
-    echo "Error."
-    exit
-fi
+my_check_rc
 echo "OK."
 
 
@@ -169,7 +212,7 @@ cd "/${BACULADIR}"
 ./bacula start
 sleep 3
 
-/sbin/bconsole -c /etc/bacula/bconsole.conf<<END_OF_DATA
+${BCONSOLE} -c /etc/bacula/bconsole.conf<<END_OF_DATA
 @output /dev/null
 messages
 @output ${TMPDIR}/log/01.log
@@ -197,7 +240,7 @@ dd if=/dev/zero of="${TMPDIR}/test/2/file_new23.dat"  bs=1024 count=400
 dd if=/dev/zero of="${TMPDIR}/test/3/file_new31.dat"  bs=1024 count=500
 
 
-/sbin/bconsole -c /etc/bacula/bconsole.conf<<END_OF_DATA
+${BCONSOLE} -c /etc/bacula/bconsole.conf<<END_OF_DATA
 @output /dev/null
 messages
 @output ${TMPDIR}/log/02.log
@@ -224,7 +267,7 @@ dd if=/dev/zero of="${TMPDIR}/test/1/file_new12.dat"  bs=1024 count=350
 dd if=/dev/zero of="${TMPDIR}/test/2/file_new24.dat"  bs=1024 count=450
 dd if=/dev/zero of="${TMPDIR}/test/3/file_new32.dat"  bs=1024 count=550
 
-/sbin/bconsole -c /etc/bacula/bconsole.conf<<END_OF_DATA
+${BCONSOLE} -c /etc/bacula/bconsole.conf<<END_OF_DATA
 @output /dev/null
 messages
 @output ${TMPDIR}/log/03.log
@@ -250,8 +293,21 @@ END_OF_DATA
 my_check_log "${TMPDIR}/log/03.log" "backup 3"
 
 
-#echo "Press Enter..."
-#read
+my_log "Run backup 4 to autochanger ..."
+${BCONSOLE} -c /etc/bacula/bconsole.conf<<END_OF_DATA
+@output /dev/null
+messages
+@output ${TMPDIR}/log/04.log
+@sleep 1
+run job="job.name.test.autochanger.1" level=Full yes
+wait
+messages
+quit
+END_OF_DATA
+
+my_check_log "${TMPDIR}/log/04.log" "backup 4 autochanger"
+
+
 rm -r -f  ${TMPDIR}/log/*
 
 
