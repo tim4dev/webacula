5ef8b242773f47a03f774fee5586218a076960f3
Yuri Timofeev <tim4dev@gmail.com>
2010-01-02 22:12:56 +0200

    autochanger

--- tests/application/controllers/StorageControllerTest.php
+++ tests/application/controllers/StorageControllerTest.php
@@ -46,4 +46,41 @@ class StorageControllerTest extends ControllerTestCase
         // http://by.php.net/manual/en/function.preg-match.php
         $this->assertQueryContentRegex('td', "/Storage1 Version:.*linux/");
     }
+
+
+    /**
+     * @group use-bconsole
+     */
+    public function testStorageUmount ()
+    {
+        print "\n" . __METHOD__ . ' ';
+        $this->dispatch('storage/act-mount/act/umount/name/storage.file.1');
+        $this->assertModule('default');
+        $this->assertController('storage');
+        $this->assertAction('act-mount');
+        $this->assertNotQueryContentRegex('table', self::ZF_pattern); // Zend Framework
+        $this->assertResponseCode(200);
+        $this->assertQueryContentContains('td', '1000 OK: main.dir');
+        $this->assertNotQueryContentRegex('td', '/Error/i');
+        $this->assertQueryContentContains('td', '3901 Device "dev.file.storage.1" (/tmp/webacula/dev) is already unmounted');
+    }
+
+
+    /**
+     * @group use-bconsole
+     */
+    public function testStorageMount ()
+    {
+        print "\n" . __METHOD__ . ' ';
+        $this->dispatch('storage/act-mount/act/mount/name/storage.file.1');
+        $this->assertModule('default');
+        $this->assertController('storage');
+        $this->assertAction('act-mount');
+        $this->assertNotQueryContentRegex('table', self::ZF_pattern); // Zend Framework
+        $this->assertResponseCode(200);
+        $this->assertQueryContentContains('td', '1000 OK: main.dir');
+        $this->assertNotQueryContentRegex('td', '/Error/i');
+        $this->assertQueryContentContains('td', '3906 File device "dev.file.storage.1" (/tmp/webacula/dev) is always mounted.');
+    }
+
 }
