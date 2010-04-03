12048fe5def77a34fbcdf46dfa4014d1d118d5dc
Yuri Timofeev <tim4dev@gmail.com>
2010-04-03 21:34:02 +0300

    fix postgresql make tables

--- install/webacula_postgresql_make_tables.sh
+++ install/webacula_postgresql_make_tables.sh
@@ -28,17 +28,13 @@ GRANT all ON wbLogBook TO ${db_user};
 GRANT all ON wbLogType TO ${db_user};
 GRANT all ON wbJobDesc TO ${db_user};
 GRANT SELECT, REFERENCES ON wbVersion TO ${db_user};
-GRANT all ON wbtmptablelist TO ${db_user};
 
 GRANT SELECT, UPDATE ON wblogbook_logid_seq TO ${db_user};
 GRANT SELECT, UPDATE ON wblogtype_typeid_seq TO ${db_user};
-GRANT SELECT, UPDATE ON wbtmptablelist_tmpid_seq TO ${db_user};
 GRANT SELECT, UPDATE ON wbjobdesc_desc_id_seq TO ${db_user};
 
 -- execute access
 GRANT EXECUTE ON FUNCTION my_clone_file(vTbl TEXT, vFileId INT, vPathId INT, vFilenameId INT, vLStat TEXT, vMD5 TEXT, visMarked INT, vFileSize INT) TO ${db_user};
-GRANT EXECUTE ON FUNCTION my_clone_filename(vTbl TEXT, vFilenameId INT, vName TEXT) TO ${db_user};
-GRANT EXECUTE ON FUNCTION my_clone_path(vTbl TEXT, vPathId INT, vPath TEXT) TO ${db_user};
 
 END-OF-DATA
 
