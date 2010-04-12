1dc14d56053661b24d39f9909d619bd9d59114b8
Yuri Timofeev <tim4dev@gmail.com>
2010-04-13 01:19:18 +0300

    add phpversion into help page

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
  *
  */
 
-define('WEBACULA_VERSION', '5.0' . ', build 2010.04.05');
+define('WEBACULA_VERSION', '5.0' . ', build 2010.04.12');
 define('BACULA_VERSION', 12); // Bacula Catalog version
 
 define('ROOT_DIR', dirname(dirname(__FILE__)) );
