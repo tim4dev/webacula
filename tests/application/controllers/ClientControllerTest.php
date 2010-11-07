ae03d9809b769d377f2dfaf1914857517c22833f
Yuri Timofeev <tim4dev@gmail.com>
2010-11-07 20:25:29 +0200

    [test] fix Client Controller Test

--- tests/application/controllers/ClientControllerTest.php
+++ tests/application/controllers/ClientControllerTest.php
@@ -26,22 +26,22 @@ class ClientControllerTest extends ControllerTestCase
 		$this->assertQueryContentContains('td', 'local.fd');
 	}
 
-	/**
-	 * @group use-bconsole
-	 */
-   public function testStatus()
-   {
-      print "\n".__METHOD__.' ';
-      $this->dispatch('client/status-client-id/id/1/name/local.fd');
+    /**
+     * @group use-bconsole
+    */
+    public function testStatus()
+    {
+        print "\n".__METHOD__.' ';
+        $this->dispatch('client/status-client-id/id/1/name/local.fd');
       $this->assertController('client');
       $this->assertAction('status-client-id');
-      //echo $this->response->outputBody(); // for debug !!!
+      //  echo $this->response->outputBody(); // for debug !!!
       $this->assertNotQueryContentRegex('table', self::ZF_pattern); // Zend Framework
       $this->assertResponseCode(200);
       $this->assertQueryContentContains('td', '1000 OK: main.dir');
       $this->assertNotQueryContentRegex('td', '/Error/i');
       $this->assertQueryContentRegex('td', "/local.fd Version:.*linux/");
-      $this->assertQueryContentRegex('td', "/Daemon started.*run since started/");
+      $this->assertQueryContentRegex('td', "/Daemon started/");
    }
 
 }
