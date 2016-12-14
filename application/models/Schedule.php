87e32599fd59b9903cd4288af23fad56bd17a466
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2016-12-14 16:42:04 -0200

    Fix month name in Schedule

--- application/models/Schedule.php
+++ application/models/Schedule.php
@@ -120,7 +120,7 @@ class Schedule
      {
     	$config = Zend_Registry::get('config');
         $array_month_short = array(0=>'Jan',1=>'Feb',2=>'Mar',3=>'Apr',4=>'May',5=>'Jun',6=>'Jul',7=>'Aug',8=>'Sep',9=>'Oct',10=>'Nov',11=>'Dec',);
-        $array_month_long = array(0=>'January',1=>'February',2=>'March',3=>'April',4=>'May',5=>'June',6=>'July',7=>'August',8=>'Sepetember',9=>'October',10=>'November',11=>'December',);
+        $array_month_long = array(0=>'January',1=>'February',2=>'March',3=>'April',4=>'May',5=>'June',6=>'July',7=>'August',8=>'September',9=>'October',10=>'November',11=>'December',);
         $array_weekday_short = array(0=>'Sun',1=>'Mon',2=>'Tue',3=>'Wed',4=>'Thu',5=>'Fri',6=>'Sat',);
         $array_weekday_long = array(0=>'Sunday',1=>'Monday',2=>'Tuesday',3=>'Wednesday',4=>'Thursday',5=>'Friday',6=>'Saturday');
         $array_weekmonth_short = array(0=>'1st',1=>'2nd',2=>'3rd',3=>'4th',4=>'5th',5=>'6th');
