4a23f299719cca73f71a4c7cd438d46d51c21b43
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-07-13 15:28:56 -0300

    Update changelog and version

--- html/index.php
+++ html/index.php
@@ -18,7 +18,7 @@
  *
  */
 
-define('WEBACULA_VERSION', '7.5.1' . ' build 30May2017');
+define('WEBACULA_VERSION', '7.5.2' . ' build 10Jul2017');
 
 define('ROOT_DIR', dirname(dirname(__FILE__)) );
 define('CACHE_DIR',  ROOT_DIR.'/data/cache' );
